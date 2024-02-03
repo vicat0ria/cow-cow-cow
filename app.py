7288443c5b2438ba80395db28a502572bff8b3d5
Ana K <kiseleva.anastasiya1405@gmail.com>
2024-02-03 15:49:24 -0800

    woomy

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 # Dependencies: Flask, Numpy, Pandas
-from flask import Flask,render_template, request 
+from flask import Flask,render_template, request, jsonify
 import pandas as pd
-import numpy as np
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+user_data = pd.DataFrame({
+    "username": ["test"],
+    "userX": [0],
+    "userY": [0],
+    "targetX": [0],
+    "targetY": [0]
+})
+
 
 app = Flask(__name__,template_folder="templates", static_url_path='/static') 
 @app.route("/") 
@@ -10,12 +20,31 @@ def index():
 
 @app.route('/receive_value', methods=['POST'])
 def receive_value():
-    data = request.get_json()
-    paragraph_value = data['value']
-    # Now you can use the paragraph_value in your Flask server
-    print(f'Received value from client: {paragraph_value}')
-    return 'Value received successfully'
+    try:
+        data = request.get_json()
+
+        global stored_data
+        stored_data = data
+        print("yay data")
+        result = {'status': 'success', 'message': 'Data stored successfully'}
+        return jsonify(result)
+
+    except Exception as e:
+        result = {'status': 'error', 'message': str(e)}
+        return jsonify(result), 500
+    
+@app.route('/get_coords', methods=['GET'])
+def get_coords():
+    try:
+        global stored_data
+        result = {'status': 'success', 'data': stored_data}
+        print(stored_data)
+        return jsonify(result)
+
+    except Exception as e:
+        result = {'status': 'error', 'message': str(e)}
+        return jsonify(result), 500
+
 
 if __name__ == '__main__': 
     app.run(debug=True) 
-

8dec0daff453f4bd2cf8c83756d95cd429541d8e
Ana K <kiseleva.anastasiya1405@gmail.com>
2024-02-03 10:49:34 -0800

    stole vaishnavi's code (not really)
    organized the files, edited index.html to reference static/script.js

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 # Dependencies: Flask, Numpy, Pandas
-import flask
+from flask import Flask,render_template, request 
 import pandas as pd
 import numpy as np
 
+app = Flask(__name__,template_folder="templates") 
+@app.route("/") 
+def hello(): 
+    return render_template('index.html') 
+
+@app.route('/process-data', methods=['POST']) 
+def process_data(): 
+    data = request.json['data'] 
+    result = sum(data) 
+    return jsonify({'result': result}) 
+  
+if __name__ == '__main__': 
+    app.run(debug=True) 

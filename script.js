4824e70befe6e9d0b709b4534e715ef82c4d2fc8
Vaishnavi Sen <vaishnavisen@Vaishnavis-MacBook-Pro.local>
2024-02-03 13:38:30 -0800

    added features for different levels

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-var pageIndex = 0;
 
-const userSize = 100;
-const targetLocation = { x: 300, y: 400 };
+var userSize;
+var targetLocation;
 let userLocation = { x: 0, y: 0 };
 let hasWon = false; 
 
-function refresh() {    
-    if (pageIndex = 1) {
-        loadGame();
-    }
-}
-
 function loadGame() {
-    pageIndex = 1; 
+    // levelDecision(1);
     document.getElementById('container').style.display = 'none';
     document.getElementById('gameCanvas').style.display = 'block';
-    document.getElementById('coordinates').style.display = 'block';
     document.getElementById('x-coordinate').innerHTML = userLocation.x; 
     document.getElementById('y-coordinate').innerHTML = userLocation.y;
     draw(); 
@@ -29,7 +21,7 @@ function draw() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // Draw user
-    ctx.fillStyle = 'pink';
+    ctx.fillStyle = 'white';
     ctx.fillRect(userLocation.x, userLocation.y, userSize, userSize);
 
     if (hasWon) {
@@ -37,13 +29,14 @@ function draw() {
         ctx.fillRect(userLocation.x, userLocation.y, userSize, userSize);
     }
 
+    levelDecision(1);
+
     // Check if the user has reached the hidden object
     if (userLocation.x == targetLocation.x &&
         userLocation.y == targetLocation.y) {
-        alert('Congratulations! You found the hidden object. You won!');
+        alert('Congratulations! You found the hidden object.');
         resetGame();
     }
-
     requestAnimationFrame(draw);
 }
 
@@ -54,7 +47,7 @@ function resetGame() {
 }
 
 document.addEventListener('keydown', (event) => {
-    const speed = 100;
+    const speed = 150;
 
     // update the location on screen 
     switch (event.key) {
@@ -75,22 +68,54 @@ document.addEventListener('keydown', (event) => {
     // customized alert for boundaries
     if (userLocation.x < 0) {
         console.log('You are hitting the left wall');
-        userLocation.x = userLocation.x + 100;
+        userLocation.x = userLocation.x + 150;
     }
-    if (userLocation.x > 700) {
+    if (userLocation.x > 500) {
         console.log('You are hitting the right wall');
-        userLocation.x = userLocation.x - 100;
+        userLocation.x = userLocation.x - 150;
     }
     if (userLocation.y < 0) {
         console.log('You are hitting the top wall');
-        userLocation.y = userLocation.y + 100;
+        userLocation.y = userLocation.y + 150;
     }
     if (userLocation.y > 500) {
         console.log('You are hitting the bottom wall');
-        userLocation.y = userLocation.y - 100;
+        userLocation.y = userLocation.y - 150;
     }
 
     // update the location: for testing purposes
     document.getElementById('x-coordinate').innerHTML = userLocation.x; 
     document.getElementById('y-coordinate').innerHTML = userLocation.y;
 });
+
+function levelDecision(level) {
+    switch(level) {
+        case 0: 
+            targetLocation = { x: 450, y: 0 };
+            userSize = 150;
+            break;
+        case 1: 
+            targetLocation = { x: 450, y: 450 };
+            userSize = 150;
+            // addWalls(2);
+            break;
+    }
+}
+
+// function addWalls(level) {
+//     switch(level) {
+//         case 2:
+//             ctx.beginPath();
+//             ctx.moveTo(0, 150);
+//             ctx.lineTo(150, 150);
+//             ctx.lineTo(150, 600);
+//             ctx.lineWidth = 10;
+//             ctx.strokeStyle = "pink";
+//             ctx.stroke();
+//             ctx.moveTo(300, 0);
+//             ctx.lineTo(300, 450);
+//             ctx.lineWidth = 10;
+//             ctx.strokeStyle = "pink";
+//             ctx.stroke();
+//     }
+// }
